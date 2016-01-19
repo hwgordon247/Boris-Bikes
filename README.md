ee7f71d9542a1d04fadb523022b671b673a128ad
Alex Avlonitis <alex@Alexs-MacBook-Pro.local>
2016-01-19 11:46:11 +0000

    added everything

--- README.md
+++
@@ -1,2 +0,0 @@
-Boris Bikes challenge
-A system like boris bikes in london for hiring bikes 
--- lib/dockingStation.rb
+++ lib/dockingStation.rb
@@ -1,10 +1,5 @@
 class DockingStation
-<<<<<<< HEAD
-
-end
-=======
   def release_bike
     
   end
 end
->>>>>>> 6f6b4d13b6a5ecc3fc2888367c7cd30aa13bc175
