b09fc22ec1f5e17f26cdcc220cd9c5912b06391f
Henry Gordon <hwgordon247@gmail.com>
2016-01-19 13:33:21 +0000

    after chapter 10

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,11 @@
+require './lib/bike.rb'
+
 class DockingStation
   def release_bike
-
+    Bike.new
   end
 end
+
+station = DockingStation.new
+bike = station.release_bike
+p bike.working?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,10 @@ describe DockingStation do
 
   it {is_expected.to respond_to(:release_bike)}
 
+
+  it 'gets a bike and expects it to work' do
+    expect((subject.release_bike).working?).to be_truthy
+  end
+
+
 end
