c2d263f28f3165a3ebe135ecf40af6a1c2f37ac4
esbaddeley <esbaddeley@gmail.com>
2016-01-19 15:20:32 +0000

    Created test for docking a bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,22 @@
 require './lib/bike.rb'
 
 class DockingStation
+
+  attr_reader :bikes
+
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+  @bikes = bike
+  end
+
 end
 
 station = DockingStation.new
 bike = station.release_bike
-p bike.working?
+station.dock(bike)
+
+p station.bikes
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,21 @@ describe DockingStation do
 
   it {is_expected.to respond_to(:release_bike)}
 
-
   it 'gets a bike and expects it to work' do
     expect((subject.release_bike).working?).to be_truthy
+  end
+
+  it {is_expected.to respond_to(:dock).with(1).argument}
+
+  it {is_expected.to respond_to(:bikes)}
+
+  before do
+    bike = subject.release_bike
+    subject.dock(bike)
+  end
+
+  it 'can see a bike' do
+    expect((subject.bikes)).to an_instance_of Bike
   end
 
 
