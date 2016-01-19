6f6b4d13b6a5ecc3fc2888367c7cd30aa13bc175
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-19 11:36:04 +0000

    Unit Test 4: Added release_bike method to DockingStation class.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require "dockingStation"
 
 describe DockingStation do
     it "Releases a bike" do
-      expect(DockingStation.release_bike).to eq "Bike released."
+      docking_station = DockingStation.new
+      expect(docking_station.release_bike).to eq "Bike released."
     end
 end
