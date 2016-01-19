67422c50fe435b20e048ac482eab3803e3c5e650
Alex Avlonitis <alex@Alexs-MacBook-Pro.local>
2016-01-19 15:29:50 +0000

    adding

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,4 +13,16 @@ describe DockingStation do
       bike = ds.release_bike
       expect(bike.working?).to be_truthy
     end
+
+    it "Can the Docking station Dock bikes" do
+      ds = DockingStation.new
+      expect(ds.respond_to?(:dock_bike)).to eq true
+    end
+
+    it "Docks the bike when the bike is returned" do
+      ds = DockingStation.new
+      bike = ds.release_bike
+      expect(ds.dock_bike(bike)).to
+    end
+
 end
