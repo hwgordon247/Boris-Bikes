e1585dc2350681a4f7db00e4c3cc3a4f25e3bf00
JoeWroe <joe_wroe@icloud.com>
2016-01-20 10:39:48 +0000

    Updated .rspec, Feature test for docking a bike with 1 argument written

--- Boris_Bikes/spec/DockingStation_spec.rb
+++ Boris_Bikes/spec/DockingStation_spec.rb
@@ -3,9 +3,12 @@ require 'DockingStation'
 describe DockingStation do
   it { should respond_to(:release_bike) }
 
-  it 'expects "release bike" to get a working bike' do
+  it 'expects "release_bike" to get a working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  context 'responds to "dock" with one argument'
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
 end
