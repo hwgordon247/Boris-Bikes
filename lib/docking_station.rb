9cab42ea353fb742b12828ccc2495e58eec0786e
arnoldmanzano <arnold.manzano@gmail.com>
2016-01-20 16:07:21 +0000

    ch12 Added exception test and passed. Changed bike.working test. Changed release_bike method.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
-
   attr_reader :bike
 
+# dockingstation assumes to have no bikes
+
   def release_bike
-    Bike.new
+    raise "No bikes available!" if @bike == nil
+    @bike
   end
 
   def dock_bike(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,10 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:release_bike) }
 
-  it { expect(subject.release_bike.working?).to eq true }
+  it 'check if bike is working' do
+    bike = Bike.new
+    expect(bike.working?).to eq true
+  end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
@@ -19,6 +22,16 @@ describe DockingStation do
     bike = Bike.new
     subject.dock_bike(bike)
     expect(subject.bike).to eq bike
+  end
+
+  it 'release a bike when it is available' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.release_bike).to eq bike
+  end
+
+  it 'raise an error' do
+    expect { subject.release_bike }.to raise_error("No bikes available!")
   end
 
 end
