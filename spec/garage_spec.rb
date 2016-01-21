636d6c8ab275b4b34957c5a6b13b4136367909b3
Henry Gordon <hwgordon247@gmail.com>
2016-01-21 16:28:53 +0000

    fixed the rspec before problem

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,23 +1,24 @@
 require 'garage'
 
 describe Garage do
-
-  bike = Bike.new
-  bike2 = Bike.new
-  ds = DockingStation.new
-  ds.dock_bike(bike)
-  ds.dock_bike(bike2, false)
-  van = Van.new
-  broken = van.collect_broken(ds)
+  before(:example) do
+    @bike = Bike.new
+    @bike2 = Bike.new
+    @ds = DockingStation.new
+    @ds.dock_bike(@bike)
+    @ds.dock_bike(@bike2, false)
+    @van = Van.new
+    @broken = @van.collect_broken(@ds)
+  end
 
   it 'takes broken bikes from the van' do
-    van.delivers_broken(subject)
-    expect(subject.broken_bikes).to eq broken
+    @van.delivers_broken(subject)
+    expect(subject.broken_bikes).to eq @broken
   end
 
   it 'is empty after the van collects' do
-    van.delivers_broken(subject)
-    van.collect_fixed(subject)
+    @van.delivers_broken(subject)
+    @van.collect_fixed(subject)
     expect(subject.broken_bikes).to be_empty
   end
 
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,33 +1,34 @@
 require 'van'
 
 describe Van do
-
-  bike = Bike.new
-  bike2 = Bike.new
-  ds = DockingStation.new
-  ds.dock_bike(bike)
-  ds.dock_bike(bike2, false)
+  before(:example) do
+    @bike = Bike.new
+    @bike2 = Bike.new
+    @ds = DockingStation.new
+    @ds.dock_bike(@bike)
+    @ds.dock_bike(@bike2, false)
+  end
 
   it 'van collect the broken bikes from the docking station' do
-    expect(subject.collect_broken(ds)).to include bike2
-    expect(subject.collect_broken(ds)).not_to include bike
+    expect(subject.collect_broken(@ds)).to include @bike2
+    expect(subject.collect_broken(@ds)).not_to include @bike
   end
 
   it 'after the van collects, ensure only working bikes in docking station' do
-    subject.collect_broken(ds)
-    expect(ds.bikes).to include bike
-    expect(ds.bikes).not_to include bike2
+    subject.collect_broken(@ds)
+    expect(@ds.bikes).to include @bike
+    expect(@ds.bikes).not_to include @bike2
   end
 
   it 'delivers broken bikes to the garage' do
     garage = Garage.new
-    subject.collect_broken(ds)
+    subject.collect_broken(@ds)
     expect(subject.delivers_broken(garage)).to be_empty
   end
 
   it 'collects fixed bikes from the garage' do
     garage = Garage.new
-    bikes_to_fix = subject.collect_broken(ds)
+    bikes_to_fix = subject.collect_broken(@ds)
     subject.delivers_broken(garage)
     expect(subject.collect_fixed(garage)).to eq bikes_to_fix
 
